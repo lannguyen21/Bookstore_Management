ff147f7c638298d63522123ccdc7e0c89be459a3
lannguyen21 <39295811+lannguyen21@users.noreply.github.com>
2018-05-21 01:31:32 +0700

    UI

--- src/tables/BookForm.java
+++ src/tables/BookForm.java
@@ -1,22 +1,23 @@
 package tables;
 
 import java.awt.EventQueue;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JFrame;
-import java.awt.Button;
-import java.awt.BorderLayout;
-import java.awt.Panel;
-import java.awt.Label;
-import javax.swing.Box;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.BoxLayout;
-import javax.swing.JDesktopPane;
+import java.awt.*;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+
 
 public class BookForm {
 
@@ -27,13 +28,23 @@ public class BookForm {
 	private JTextField Publisher;
 	private JTextField quantity;
 	private JTextField Type;
+	private JTable table;
 
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/QLS?autoReconnect=true&useSSL=false";
+
+	// Database credentials
+	static final String USER = "root";
+	static final String PASS = "kannakamui";
+	
+	
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				
 				try {
 					BookForm window = new BookForm();
 					window.frame.setVisible(true);
@@ -49,14 +60,81 @@ public class BookForm {
 	 */
 	public BookForm() {
 		initialize();
+		
 	}
 
+
+	
+	 public static void main(String[] args) {
+		Connection con = null;
+		Statement st = null;
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			con = DriverManager.getConnection(DB_URL,USER , PASS);
+
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			st = con.createStatement();
+			String sql;
+			sql = "SELECT Num, BookID, BookName, Author, BookType, Publisher FROM BookInfo";
+			ResultSet rs = st.executeQuery(sql);
+
+			// STEP 5: Extract data from result set
+			while (rs.next()) {
+				// Retrieve by column name
+				int Num = rs.getInt("Num");
+				int BookID = rs.getInt("BookID");
+				String BookName = rs.getString("BookName");
+				String Author = rs.getString("Author");
+				String BookType = rs.getString("BookType");
+				String Publisher = rs.getString("Publisher");
+
+				// Display values
+				System.out.print("Number: " + Num);
+				System.out.print(", BookID: " + BookID);
+				System.out.print(", BookName: " + BookName);
+				System.out.print(", Author: " + Author);
+				System.out.print(", BookType: " + BookType);
+				System.out.println(", Publisher: " + Publisher);
+			}
+			// STEP 6: Clean-up environment
+			rs.close();
+			st.close();
+			con.close();
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+		} finally {
+			// finally block used to close resources
+			try {
+				if (st != null)
+					st.close();
+			} catch (SQLException se2) {
+			} // nothing we can do
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} // end finally try
+		} // end try
+		System.out.println("Goodbye!");
+		
+	}
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 813, 478);
+		frame.setBounds(100, 100, 905, 519);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -133,5 +211,32 @@ public class BookForm {
 		btnSearch.setBackground(new Color(245, 255, 250));
 		btnSearch.setBounds(266, 377, 91, 27);
 		frame.getContentPane().add(btnSearch);
+		
+		JPanel panelTable = new JPanel();
+		panelTable.setToolTipText("Table");
+		panelTable.setBounds(394, 28, 474, 419);
+		frame.getContentPane().add(panelTable);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panelTable.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+			},
+			new String[] {
+				"No.", "BookID", "Name", "Author", "Type", "Publisher"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(40);
+		table.getColumnModel().getColumn(1).setPreferredWidth(85);
+		table.getColumnModel().getColumn(2).setPreferredWidth(113);
+		table.getColumnModel().getColumn(3).setPreferredWidth(97);
+		table.getColumnModel().getColumn(4).setPreferredWidth(82);
+		table.getColumnModel().getColumn(5).setPreferredWidth(93);
+		scrollPane.setViewportView(table);
 	}
 }
--- src/tables/BookInfo.java
+++ src/tables/BookInfo.java
@@ -8,45 +8,66 @@ public class BookInfo {
 	private String type;
 	private String publisher;
 	
+	BookInfo(){
+	}
+
+	public BookInfo(int no, int bookID, String name, String author, String type, String publisher) {
+		super();
+		No = no;
+		this.bookID = bookID;
+		this.name = name;
+		this.author = author;
+		this.type = type;
+		this.publisher = publisher;
+	}
+
 	public int getNo() {
 		return No;
 	}
+
 	public void setNo(int no) {
 		No = no;
 	}
-	
+
 	public int getBookID() {
 		return bookID;
 	}
+
 	public void setBookID(int bookID) {
 		this.bookID = bookID;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
-	
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
-	
+
 	public String getPublisher() {
 		return publisher;
 	}
+
 	public void setPublisher(String publisher) {
 		this.publisher = publisher;
 	}
+	
+	
 }
